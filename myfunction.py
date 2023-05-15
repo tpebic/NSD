70f225aaca88b4198a6f88b5f5057cb539a732d3
tpebic <32349942+tpebic@users.noreply.github.com>
2023-05-15 10:29:16 -0500

    Files

---
+++ myfunction.py
@@ -0,0 +1,12 @@
+def myName(firstname, lastname):
+    print(f"My name is {firstname} {lastname} from myName function.")
+
+
+def myAddfunc(num1, num2):
+    result = num1 + num2
+    print(f"The sum of myAddfunc is: {result}.")
+
+
+def myMulfunc(num1, num2):
+    result = num1 * num2
+    print(f"The product of myMulfunc is: {result}.")
